00c85d7179041bd93831dfcb9f9c8dd71b011216
박응용 <pahkey@gmail.com>
2022-10-21 00:28:44 +0900

    v1.07

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
+## FastAPI 서버 실행
+* uvicorn main:app --reload
+
 ## Svelte 서버 실행
 * npm run dev
---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/hello")
+def hello():
+    return {"message": "안녕하세요 파이보"}
