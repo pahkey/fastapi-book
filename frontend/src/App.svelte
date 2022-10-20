0e5b06d5989d21a2ed191a0c428dc7099121d8ff
박응용 <pahkey@gmail.com>
2022-10-21 01:06:18 +0900

    v2.05.1

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,11 +1,19 @@
 <script>
-  let message;
+  let question_list = []
 
-  fetch("http://127.0.0.1:8000/hello").then((response) => {
-    response.json().then((json) => {
-      message = json.message;
-    });
-  });
+  function get_question_list() {
+    fetch("http://127.0.0.1:8000/api/question/list").then((response) => {
+      response.json().then((json) => {
+        question_list = json
+      })
+    })
+  }
+
+  get_question_list()
 </script>
 
-<h1>{message}</h1>
+<ul>
+  {#each question_list as question}
+    <li>{question.subject}</li>
+  {/each}
+</ul>
