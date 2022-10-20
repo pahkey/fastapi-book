59bf63d9e7a0df718111bbfd692bad7c12c27b04
박응용 <pahkey@gmail.com>
2022-10-21 01:10:16 +0900

    v2.05.2

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,19 +1,10 @@
 <script>
-  let question_list = []
+  import Router from 'svelte-spa-router'
+  import Home from "./routes/Home.svelte"
 
-  function get_question_list() {
-    fetch("http://127.0.0.1:8000/api/question/list").then((response) => {
-      response.json().then((json) => {
-        question_list = json
-      })
-    })
+  const routes = {
+    '/': Home,
   }
-
-  get_question_list()
 </script>
 
-<ul>
-  {#each question_list as question}
-    <li>{question.subject}</li>
-  {/each}
-</ul>
+<Router {routes}/>
---
+++ frontend/src/routes/Home.svelte
@@ -0,0 +1,19 @@
+<script>
+    let question_list = []
+
+    function get_question_list() {
+        fetch("http://127.0.0.1:8000/api/question/list").then((response) => {
+            response.json().then((json) => {
+                question_list = json
+            })
+        })
+    }
+
+    get_question_list()
+</script>
+
+<ul>
+    {#each question_list as question}
+        <li>{question.subject}</li>
+    {/each}
+</ul>
