0bff689d10cb4c9844f48b9a7fff1a24fa7bbcf9
박응용 <pahkey@gmail.com>
2022-10-21 01:03:24 +0900

    v2.04.4

---
+++ domain/question/question_crud.py
@@ -0,0 +1,9 @@
+from models import Question
+from sqlalchemy.orm import Session
+
+
+def get_question_list(db: Session):
+    question_list = db.query(Question)\
+        .order_by(Question.create_date.desc())\
+        .all()
+    return question_list
--- domain/question/question_router.py
+++ domain/question/question_router.py
@@ -2,8 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from database import get_db
-from domain.question import question_schema
-from models import Question
+from domain.question import question_schema, question_crud
 
 router = APIRouter(
     prefix="/api/question",
@@ -12,5 +11,5 @@ router = APIRouter(
 
 @router.get("/list", response_model=list[question_schema.Question])
 def question_list(db: Session = Depends(get_db)):
-    _question_list = db.query(Question).order_by(Question.create_date.desc()).all()
+    _question_list = question_crud.get_question_list(db)
     return _question_list
