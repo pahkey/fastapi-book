8ac8908580766424f0fb9105cc6f49b603afd8db
박응용 <pahkey@gmail.com>
2022-10-21 00:54:35 +0900

    v2.04.2

--- database.py
+++ database.py
@@ -10,3 +10,11 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- domain/question/question_router.py
+++ domain/question/question_router.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 
-from database import SessionLocal
+from database import get_db
 from models import Question
 
 router = APIRouter(
@@ -9,8 +10,6 @@ router = APIRouter(
 
 
 @router.get("/list")
-def question_list():
-    db = SessionLocal()
+def question_list(db: Session = Depends(get_db)):
     _question_list = db.query(Question).order_by(Question.create_date.desc()).all()
-    db.close()
     return _question_list
