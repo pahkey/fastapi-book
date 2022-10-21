22b1ed505be7e27630f3aecf8fc7c23db0f60681
박응용 <pahkey@gmail.com>
2022-10-21 10:43:20 +0900

    v3.06

--- frontend/src/routes/Home.svelte
+++ frontend/src/routes/Home.svelte
@@ -40,6 +40,9 @@
             <td>{ total - ($page * size) - i }</td>
             <td>
                 <a use:link href="/detail/{question.id}">{question.subject}</a>
+                {#if question.answers.length > 0 }
+                <span class="text-danger small mx-2">{question.answers.length}</span>
+                {/if}
             </td>
             <td>{moment(question.create_date).format("YYYY년 MM월 DD일 hh:mm a")}</td>
         </tr>
