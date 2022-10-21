756b176144f79efe7a3516e34c49c19d908307a6
박응용 <pahkey@gmail.com>
2022-10-21 14:13:43 +0900

    v3.15

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
+from starlette.responses import FileResponse
+from starlette.staticfiles import StaticFiles
 
 from domain.answer import answer_router
 from domain.question import question_router
@@ -23,3 +25,9 @@ app.add_middleware(
 app.include_router(question_router.router)
 app.include_router(answer_router.router)
 app.include_router(user_router.router)
+app.mount("/assets", StaticFiles(directory="frontend/dist/assets"))
+
+
+@app.get("/")
+def index():
+    return FileResponse("frontend/dist/index.html")
